7cd76eafcbc2dc8d4b0853c61254c3246404c5fd
MuzaffarShaikh0201 <muzaffarshaikh0201@gmail.com>
2022-10-14 08:51:12 +0530

    Updated Input, Messages and Messages components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import UserImg from '../assets/profile_pic.png';
 import MemeImg from '../assets/meme_img.jpg';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
-const Message = () => {
+const Message = ({message}) => {
+  const {currentUser} = useContext(AuthContext);
+  const {data} = useContext(ChatContext);
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({behavior: "smooth"});
+  }, [message]);
+
+  const time = () => {
+    let date = new Date(message.date.seconds * 1000);
+    return `${date.getHours()}:${date.getMinutes()}`
+  }
+
+  const timeStr = time();
+
   return (
-    <div className="message owner">
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
         <div className="message-info">
-            <img src={UserImg} alt="" />
-            <span>just now</span>
+            <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
+            <span>{timeStr}</span>
         </div>
         <div className="message-content">
-            <p>hello</p>
-            <img src={MemeImg} alt="" />
+            <p>{message.text}</p>
+            {message.img && <img src={message.img} alt="" />}
         </div>
     </div>
   );
